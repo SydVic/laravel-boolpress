7eabee94f4b9bf2b235b86dd8f8c2d6116e5ff15
SydVic <fabrizio.bocca27@gmail.com>
2022-07-05 15:56:12 +0200

    aggiunto vue per visualizzazione parte guest

---
+++ resources/js/components/WorkInProgress.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>WORK IN PROGRESS</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WorkInProgress'
+}
+</script>
+
+<style>
+
+</style>
---
+++ resources/js/views/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>VUE GUEST</h2>
+    <WorkInProgress />
+  </div>
+</template>
+
+<script>
+import WorkInProgress from '../components/WorkInProgress.vue';
+
+export default {
+  name: 'App',
+  components: {
+    WorkInProgress
+  }
+}
+</script>
+
+<style>
+
+</style>
