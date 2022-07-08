7bafac5d80f22f8593418b7a51c67400df56411c
SydVic <fabrizio.bocca27@gmail.com>
2022-07-08 09:11:59 +0200

    aggiunta relazione tra UserDetails e User

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -3,15 +3,23 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use App\userDetails;
 
 class HomeController extends Controller
 {
     public function index() {
         $user = Auth::user();
-        // se non ho capito male la relazione è con il model, quindi devi usare ->UserDetails che è il nome del model
-        $user_details = $user->UserDetails;
+        // se non ho capito male la relazione è con il model, quindi devi usare ->userDetails che è il nome del model
+        $user_details = $user->userDetails;
+
+        // si può fare anche il percorso contrario, prelevare User partendo da UserDetails
+        // ti serve però l'id dello user che vuoi prelevare
+        // $user_details = UserDetails::find(1);
+        // $user = $user_details->user;
+        // dd($user);
+
         return view('admin.home', compact('user', 'user_details'));
     }
 }
--- app/UserDetails.php
+++ app/UserDetails.php
@@ -8,4 +8,10 @@ class userDetails extends Model
 {
     // serve a specificare la tabella quando non corrisponde 
     protected $table = 'users_details';
+
+    // ad ogni riga di users_details corrisponde un solo user
+    public function user() {
+        // belongs to perchè è la tabella 'secondaria' che dipende dalla tabella user, va nella tabella che la foreign key
+        return $this->belongsTo('App\User');
+    }
 }
